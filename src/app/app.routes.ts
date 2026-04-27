32aadc767968aa673f4a94a4bf611e3a11ebc54a
Alan Rodrigo Abalos <rodrigoabalos9@gmail.com>
2026-04-26 22:12:33 -0300

    feat: configuracion de lazy loading para customer y admin

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'reservar',
+    loadChildren: () => import('./features/customer/customer.module').then(m => m.CustomerModule)
+  },
+  {
+    path: 'admin',
+    loadChildren: () => import('./features/admin/admin.module').then(m => m.AdminModule)
+  },
+  { path: '', redirectTo: 'reservar', pathMatch: 'full' },
+  { path: '**', redirectTo: 'reservar' }
+];
--- src/app/features/customer/customer-routing.module.ts
+++ src/app/features/customer/customer-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BookingFormComponent } from './pages/booking-form/booking-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: BookingFormComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
